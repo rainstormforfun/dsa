26c8155b3f34b8850e2bae5c1c0b143434f5908e
rain <rain@ubuntu-20.04.rain.virtualbox.org>
2024-03-23 15:37:07 +0800

    selection_loop.c

---
+++ sorting/selection_loop.c
@@ -0,0 +1,70 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h> 
+#define SIZE 50
+
+void num_generator(int *arr, int len);
+void selection_recursion(int *arr, int len);
+void num_printf(int *arr, int len);
+
+int main(void)
+{
+	int arr[SIZE];
+
+	num_generator(arr, SIZE);
+
+	selection_recursion(arr, SIZE);
+
+	num_printf(arr, SIZE);
+
+	return EXIT_SUCCESS;
+}
+
+void num_generator(int *arr, int len)
+{
+	srand(time(NULL));
+	for (int i = 0; i < len; i++)	
+	{
+		arr[i] = rand() % 100;
+	}	
+	
+	return;
+}	
+
+void selection_recursion(int *arr, int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		int min = arr[i];
+		int index = i;
+
+		for (int j = i; j < len; j++)
+		{
+			if (arr[j] < min)
+			{
+				min = arr[j];
+				index = j;
+			}
+		}
+
+		if (index != i)
+		{
+			arr[i] = arr[i] ^ arr[index];
+			arr[index] = arr[i] ^ arr[index];
+			arr[i] = arr[i] ^ arr[index];
+		}
+	}
+
+	return;
+}
+
+void num_printf(int *arr, int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		printf("%d ", arr[i]);
+	}	
+	printf("\n");
+
+	return;
+}
