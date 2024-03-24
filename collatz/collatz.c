47e7e3b368b5dd8ee9c25245fc96f9992b61a7b0
rain <rain@ubuntu-20.04.rain.virtualbox.org>
2024-03-24 14:23:19 +0800

    collatz

--- collatz/collatz.c
+++ collatz/collatz.c
@@ -5,7 +5,7 @@ int main(void)
 {
 	int max_count = 0;
 	int index = 1;
-	for (int i = 1; i <= 1000; i++)
+	for (int i = 1; i <= 100; i++)
 	{
 		int result = i;
 		int count = 0;
@@ -26,15 +26,20 @@ int main(void)
 		}	
 	}
 
-	printf("%d\n", index);
+	printf("All numbers are satisfied with collatz conjecture!\n");
+
+	printf("%d has the longest sequence!\n", index);
+	printf("The sequence is: \n");
+	printf("%d ", index);
 	for (int i = 0; i < max_count; i++)
 	{
 		if (index % 2 == 0)
 			index = index / 2;
 		else
 			index = index * 3 + 1;
-		printf("%d\n", index);
+		printf("%d ", index);
 	}
+	printf("\n");
 	return EXIT_SUCCESS;
 }
 
